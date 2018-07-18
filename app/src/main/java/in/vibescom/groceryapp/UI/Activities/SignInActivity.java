90c8a3cfe40e487e35dc9e2ef2e0ef8dd6adebba
Prithvesh <prithvesh1994@gmail.com>
2018-07-18 17:10:22 +0530

    Added toast to SignIn Activity

--- app/src/main/java/in/vibescom/groceryapp/UI/Activities/SignInActivity.java
+++ app/src/main/java/in/vibescom/groceryapp/UI/Activities/SignInActivity.java
@@ -12,6 +12,7 @@ public class SignInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
+
         Toast.makeText(this, "Signin Activity", Toast.LENGTH_SHORT).show();
     }
 
