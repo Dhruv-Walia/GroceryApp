1c4bdd33a8882e6b14be562f2e8d06073e87133e
Prithvesh <prithvesh1994@gmail.com>
2018-07-18 17:43:10 +0530

    Added the sigin and signup activies in Activities Foder with res/layouts and values/strings.xml and added a tag in Manifestfile

---
+++ app/src/main/java/in/vibescom/groceryapp/UI/Activities/Contact.java
@@ -0,0 +1,46 @@
+package in.vibescom.groceryapp.UI.Activities;
+
+import android.content.Context;
+
+/**
+ * Created by HP on 7/24/2015.
+ */
+public class Contact  {
+
+    String name,uname,email,pass;
+
+    public void setName(String str1){
+        this.name=name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setEmail(String str2){
+        this.email=email;
+    }
+
+    public String getEmail(){
+        return this.email;
+    }
+
+    public void setUname(String str3){
+        this.uname=uname;
+    }
+
+    public String getUname(){
+        return this.uname;
+    }
+
+    public void setPass(String strpass1){
+        this.pass=pass;
+    }
+
+    public String getPass(){
+        return this.pass;
+    }
+
+
+
+}
