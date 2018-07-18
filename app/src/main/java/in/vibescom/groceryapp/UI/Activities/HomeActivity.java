7eb81c5d74543388b387e1ee84c483a85bc692fb
Prithvesh <prithvesh1994@gmail.com>
2018-07-18 17:01:45 +0530

    Revert "Toast added"
    This reverts commit 58493cd

--- app/src/main/java/in/vibescom/groceryapp/UI/Activities/HomeActivity.java
+++ app/src/main/java/in/vibescom/groceryapp/UI/Activities/HomeActivity.java
@@ -3,7 +3,6 @@ package in.vibescom.groceryapp.UI.Activities;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Toast;
 
 import in.vibescom.groceryapp.R;
 
@@ -14,6 +13,5 @@ public class HomeActivity extends AppCompatActivity{
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        Toast.makeText(HomeActivity.this,"Home Activity",Toast.LENGTH_SHORT).show();
     }
 }
