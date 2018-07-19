d7e645c89eb73864028c847f1d96f3cd71980d7b
Prithvesh <prithvesh1994@gmail.com>
2018-07-19 18:40:16 +0530

    Revert "SignInActivity(MainActivity and activity_main.xml) and SignUpActivity with ui"
    This reverts commit 38ca68f4e9574bf9dd819cd9d9f3f150d37d3699.
    
    # Conflicts:
    #	app/src/main/java/in/vibescom/groceryapp/UI/Activities/SignUpActivity.java

--- app/src/main/java/in/vibescom/groceryapp/UI/Activities/SignUpActivity.java
+++ app/src/main/java/in/vibescom/groceryapp/UI/Activities/SignUpActivity.java
@@ -1,36 +1,19 @@
 package in.vibescom.groceryapp.UI.Activities;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
-public class SignUpActivity extends AppCompatActivity {
+import in.vibescom.groceryapp.R;
 
+
+public class SignUpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
-        Button auser = findViewById(R.id.a_user);
-        Button signup = findViewById(R.id.signUpBtn);
+        setContentView(R.layout.activity_signup);
 
-        signup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(SignUpActivity.this,"you are signed in", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        auser.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent a_i = new Intent(SignUpActivity.this,MainActivity.class);
-                startActivity(a_i);
-            }
-        });
+        Toast.makeText(this, "SignUp Activity", Toast.LENGTH_SHORT).show();
     }
-
 }
