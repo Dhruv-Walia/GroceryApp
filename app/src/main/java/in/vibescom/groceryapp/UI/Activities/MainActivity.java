38ca68f4e9574bf9dd819cd9d9f3f150d37d3699
Prithvesh <prithvesh1994@gmail.com>
2018-07-19 16:17:59 +0530

    SignInActivity(MainActivity and activity_main.xml) and SignUpActivity with ui

---
+++ app/src/main/java/in/vibescom/groceryapp/UI/Activities/MainActivity.java
@@ -0,0 +1,47 @@
+package in.vibescom.groceryapp.UI.Activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView create,forgot;
+    Button sign;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        create = findViewById(R.id.createAccount);
+        sign = findViewById(R.id.loginBtn);
+        forgot = findViewById(R.id.forgot_password);
+
+        create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent cre_i = new Intent(MainActivity.this,SignUpActivity.class);
+                startActivity(cre_i);
+            }
+        });
+
+        sign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"you are logged into the activity", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        forgot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent for_i = new Intent(MainActivity.this,SignUpActivity.class);
+                startActivity(for_i);
+            }
+        });
+    }
+}
--- app/src/main/java/in/vibescom/groceryapp/UI/Activities/SignUpActivity.java
+++ app/src/main/java/in/vibescom/groceryapp/UI/Activities/SignUpActivity.java
@@ -1,19 +1,36 @@
 package in.vibescom.groceryapp.UI.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
-import in.vibescom.groceryapp.R;
-
-
 public class SignUpActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signup);
+        setContentView(R.layout.activity_sign_up);
+        Button auser = findViewById(R.id.a_user);
+        Button signup = findViewById(R.id.signUpBtn);
 
-        Toast.makeText(this, "SignUp Activity", Toast.LENGTH_SHORT).show();
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(SignUpActivity.this,"you are signed in", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        auser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent a_i = new Intent(SignUpActivity.this,MainActivity.class);
+                startActivity(a_i);
+            }
+        });
     }
+
 }
