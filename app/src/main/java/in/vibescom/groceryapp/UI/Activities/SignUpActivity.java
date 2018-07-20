e7bc6bca219760d3b1d2d97dfde2b04aea67bda7
dhruv864 <dhruv.walia@vibescom.in>
2018-07-20 10:05:54 +0530

    Helpers,Profile and Resources

--- app/src/main/java/in/vibescom/groceryapp/UI/Activities/SignUpActivity.java
+++ app/src/main/java/in/vibescom/groceryapp/UI/Activities/SignUpActivity.java
@@ -16,23 +16,7 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-        Button auser = findViewById(R.id.a_user);
-        Button signup = findViewById(R.id.signUpBtn);
 
-        signup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(SignUpActivity.this,"you are signed in", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        auser.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent a_i = new Intent(SignUpActivity.this,MainActivity.class);
-                startActivity(a_i);
-            }
-        });
     }
 
 }
