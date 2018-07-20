45b58c21991db422bbab4f7d78592601a4624e36
dhruv864 <dhruv.walia@vibescom.in>
2018-07-20 17:18:43 +0530

    Profile and Dashboard

--- app/src/main/java/in/vibescom/groceryapp/UI/Fragments/ProfileFragment.java
+++ app/src/main/java/in/vibescom/groceryapp/UI/Fragments/ProfileFragment.java
@@ -1,14 +1,18 @@
 package in.vibescom.groceryapp.UI.Fragments;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import in.vibescom.groceryapp.R;
 
-public class DashboardFragment extends Fragment {
+public class ProfileFragment extends Fragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -18,8 +22,14 @@ public class DashboardFragment extends Fragment {
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_profile, container, false);
+        ButterKnife.bind(this,rootView);
 
         return rootView;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
     }
 }
